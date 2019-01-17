6f5e506c3c9d2b4635c5a8399a87a0ee01fd7d98
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2019-01-17 14:22:28 +0100

    Creade: Manual Controls Demo.

--- src/manual-control/index.js
+++ src/manual-control/index.js
@@ -1,3 +1,15 @@
 (function() {
-  console.log('hello world!');
+  const lightBtn = document.getElementById('lightBtn');
+  const darkBtn = document.getElementById('darkBtn');
+
+  lightBtn.addEventListener('click', setLightTheme);
+  darkBtn.addEventListener('click', setDarkTheme);
+
+  function setLightTheme() {
+    document.body.className = 'light-theme';
+  }
+
+  function setDarkTheme() {
+    document.body.className = 'dark-theme';
+  }
 })();
