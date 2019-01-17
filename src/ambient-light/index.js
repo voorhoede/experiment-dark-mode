4bb9c4fd2a49dee37c6a06ea01bdf14bb33afba4
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2019-01-17 12:38:24 +0100

    Create: Ambient Light Demo.

--- src/ambient-light/index.js
+++ src/ambient-light/index.js
@@ -1,3 +1,25 @@
 (function() {
-  console.log('hello world!');
+  if ('AmbientLightSensor' in window) {
+    try {
+      const sensor = new AmbientLightSensor();
+
+      sensor.onreading = () => {
+        const illuminance = sensor.illuminance;
+
+        if (illuminance < 20) {
+          document.body.className = 'dark-theme';
+        } else if (illluminance > 30) {
+          document.body.className = 'light-theme';
+        }
+      };
+
+      sensor.onerror = e => {
+        console.log(e.error.name, e.error.message);
+      };
+
+      sensor.start();
+    } catch (e) {
+      console.error(e);
+    }
+  }
 })();
