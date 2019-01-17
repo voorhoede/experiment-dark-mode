fd35969fe3babbcd099417beb79f0262fc7cd86c
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2019-01-17 17:08:14 +0100

    Geolocation v2

---
+++ src/geolocation/original.js
@@ -0,0 +1,46 @@
+(function() {
+  const clock = document.getElementById('clock');
+
+  function logData(currentTime, sunrise, sunset) {
+    console.table({
+      currentTime: String(currentTime),
+      sunrise: String(sunrise),
+      sunset: String(sunset),
+    });
+  }
+
+  function setLightTheme() {
+    document.body.className = 'light-theme';
+  }
+
+  function setDarkTheme() {
+    document.body.className = 'dark-theme';
+  }
+
+  function updateCurrentTheme(currentTime, sunrise, sunset) {
+    currentTime > sunrise && currentTime < sunset
+      ? setLightTheme()
+      : setDarkTheme();
+    logData(currentTime, sunrise, sunset);
+  }
+
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(position => {
+      let currentTime = new Date();
+      const { latitude, longitude } = position.coords;
+
+      setInterval(() => {
+        // const currentTime = new Date();
+        currentTime.setHours(currentTime.getHours() + 1);
+        clock.innerHTML = `${currentTime.getHours()}:59`;
+        const { sunrise, sunset } = SunCalc.getTimes(
+          currentTime,
+          latitude,
+          longitude,
+        );
+
+        updateCurrentTheme(currentTime, sunrise, sunset);
+      }, 1000);
+    });
+  }
+})();
