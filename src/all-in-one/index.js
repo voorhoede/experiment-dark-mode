635444d012b4bfa3b5143a5dd38ae6344a3e31a3
Vincent van Dijck <vincent@voorhoede.nl>
2019-01-21 10:38:49 +0100

    chore(all-in-one) better comments

--- src/all-in-one/index.js
+++ src/all-in-one/index.js
@@ -1,52 +1,77 @@
 (function() {
+  // Element variables
   const buttonThemeLight = document.getElementById('themeLight');
   const buttonThemeDark = document.getElementById('themeDark');
   const buttonThemeAuto = document.getElementById('themeAuto');
   const buttonThemeAmbient = document.getElementById('optionAmbient');
   const buttonThemeLocation = document.getElementById('optionLocation');
   const buttonThemeSystem = document.getElementById('optionSystem');
-
   const themeSettings = document.querySelector('[data-theme-settings]');
   const themeAutoOptions = document.querySelector('[data-theme-auto-options]');
   const speedUpButton = document.querySelector('[data-speed-up-timer]');
   const clock = document.getElementById('clock');
-  let timer = null;
-  let currentTime = new Date();
 
-  speedUpButton.addEventListener('click', adjustTimer, false);
-  themeSettings.addEventListener('click', showSettings, false);
+  // Events
   buttonThemeLight.addEventListener('click', useLightTheme, false);
   buttonThemeDark.addEventListener('click', useDarkTheme, false);
   buttonThemeAuto.addEventListener('click', showAutoOptions, false);
   buttonThemeAmbient.addEventListener('click', useAmbientLight, false);
   buttonThemeLocation.addEventListener('click', useLocation, false);
   buttonThemeSystem.addEventListener('click', useSystem, false);
+  speedUpButton.addEventListener('click', adjustTimer, false);
+  themeSettings.addEventListener('click', showSettings, false);
 
-  // Set light theme
+  let timer = null;
+  let currentTime = new Date();
+
+  /**
+   * setLightTheme
+   *
+   * Sets colors to a light theme.
+   */
   function setLightTheme() {
     document.body.classList.add('theme--light');
     document.body.classList.remove('theme--dark');
   }
 
-  // Set dark theme
+  /**
+   * setDarkTheme
+   *
+   * Sets colors to a dark theme.
+   */
   function setDarkTheme() {
     document.body.classList.add('theme--dark');
     document.body.classList.remove('theme--light');
   }
 
-  // Toggle the theme menu
+  /**
+   * showSettings
+   *
+   * Toggle the demo theme settings menu.
+   */
   function showSettings() {
     themeSettings.getAttribute('data-theme-settings') === 'hidden'
       ? themeSettings.setAttribute('data-theme-settings', 'show')
       : themeSettings.setAttribute('data-theme-settings', 'hidden');
   }
 
-  // Show extra options when 'Auto' is selected.
+  /**
+   * showAutoOptions
+   *
+   * Show extra options when 'Auto' is selected.
+   */
   function showAutoOptions() {
     themeAutoOptions.setAttribute('data-theme-auto-options', 'show');
   }
 
-   // User selected light theme.
+  /**
+   * useLightTheme
+   *
+   * User selected light theme manually.
+   * - 'Auto' options are hidden
+   * - Timer and clock are reset
+   * - Run setLightTheme()
+   */
   function useLightTheme() {
     themeAutoOptions.setAttribute('data-theme-auto-options', 'hide');
     clearInterval(timer);
@@ -54,7 +79,14 @@
     setLightTheme();
   }
 
-  // User selected dark theme.
+  /**
+   * useDarkTheme
+   *
+   * User selected dark theme manually.
+   * - 'Auto' options are hidden
+   * - Timer and clock are reset
+   * - Run setDarkTheme()
+   */
   function useDarkTheme() {
     themeAutoOptions.setAttribute('data-theme-auto-options', 'hide');
     clearInterval(timer);
@@ -62,7 +94,13 @@
     setDarkTheme();
   }
 
-  // Use ambient light sensor for theme.
+  /**
+   * useAmbientLight
+   *
+   * Use ambient light sensor for theme switching.
+   *
+   * Ref: https://developer.mozilla.org/en-US/docs/Web/API/AmbientLightSensor
+   */
   function useAmbientLight() {
     const sensor = new AmbientLightSensor();
     const illuminance = sensor.illuminance;
@@ -83,7 +121,13 @@
     sensor.start();
   }
 
-  // Use location for theme.
+  /**
+   * useLocation
+   *
+   * Use geolocation for theme switching.
+   *
+   * Ref: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation
+   */
   function useLocation() {
     const options = {
       enableHighAccuracy: true,
@@ -107,13 +151,24 @@
     }
   }
 
-  // Use system setting for theme.
+  /**
+   * useSystem
+   *
+   * When available, use system setting for theme.
+   * Removes any previously set theme classes.
+   *
+   * Ref: https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
+   */
   function useSystem() {
     document.body.classList.remove('theme--dark');
     document.body.classList.remove('theme--light');
   }
 
-  // Speed up time for demo purposes
+  /**
+   * adjustTimer
+   *
+   * When 'location' is use for theme switching we can speed up time for demo purposes.
+   */
   function adjustTimer() {
     timer = setInterval(() => {
       currentTime.setHours(currentTime.getHours() + 1);
