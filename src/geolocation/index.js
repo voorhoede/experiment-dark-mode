b187cfba04e57378e092257a99461b28580c8ab4
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2019-01-24 16:09:33 +0100

    Create a IP fallback for the geolocation demo.

--- src/geolocation/index.js
+++ src/geolocation/index.js
@@ -4,6 +4,8 @@
   const sunsetClock = document.getElementById('sunset');
   const output = document.querySelector('[data-demo-output]');
 
+  const ACCESS_KEY = 'f548fc3dba5f99a9a89d5c4e114c56e3'; // This is 100% safe, trust me.
+
   function setLightTheme() {
     document.body.classList.add('theme--light');
     document.body.classList.remove('theme--dark');
@@ -20,27 +22,50 @@
       : setDarkTheme();
   }
 
+  function useGeolocation(position) {
+    const { latitude, longitude } = position.coords;
+    output.innerHTML = 'Supported: Permission Granted';
+    updateClocks(latitude, longitude);
+  }
+
+  function useIPAddress() {
+    fetch(`http://api.ipapi.com/check?access_key=${ACCESS_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        const { latitude, longitude } = data;
+        output.innerHTML = 'Supported: Permission Denied';
+        updateClocks(latitude, longitude);
+      })
+      .catch(err => {
+        output.innerHTML = 'Not supported';
+      });
+  }
+
+  function updateClocks(latitude, longitude) {
+    let currentTime = new Date();
+
+    setInterval(() => {
+      currentTime.setHours(currentTime.getHours() + 1);
+      clock.innerHTML = `${currentTime.getHours()}:59`;
+      const { sunrise, sunset } = SunCalc.getTimes(
+        currentTime,
+        latitude,
+        longitude,
+      );
+
+      sunriseClock.innerHTML = `${sunrise.getHours()}:${sunrise.getMinutes()}`;
+      sunsetClock.innerHTML = `${sunset.getHours()}:${sunset.getMinutes()}`;
+      updateCurrentTheme(currentTime, sunrise, sunset);
+    }, 1000);
+  }
+
   if ('geolocation' in navigator) {
-    output.innerHTML = 'Supported';
-    navigator.geolocation.getCurrentPosition(position => {
-      let currentTime = new Date();
-      const { latitude, longitude } = position.coords;
-
-      setInterval(() => {
-        currentTime.setHours(currentTime.getHours() + 1);
-        clock.innerHTML = `${currentTime.getHours()}:59`;
-        const { sunrise, sunset } = SunCalc.getTimes(
-          currentTime,
-          latitude,
-          longitude,
-        );
-
-        sunriseClock.innerHTML = `${sunrise.getHours()}:${sunrise.getMinutes()}`;
-        sunsetClock.innerHTML = `${sunset.getHours()}:${sunset.getMinutes()}`;
-        updateCurrentTheme(currentTime, sunrise, sunset);
-      }, 1000);
-    });
+    navigator.geolocation.getCurrentPosition(
+      position => useGeolocation(position),
+      useIPAddress,
+    );
   } else {
     output.innerHTML = 'Not supported';
+    useIPAddress();
   }
 })();
